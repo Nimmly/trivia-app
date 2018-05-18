23cc12341aebb4060d1ba8d42d8c483e26eccceb
Nimmly <marko.v.popovic@gmail.comgot config --global user.name Nimmlygit config --global user.email marko.v.popovic@gmail.com>
2018-05-18 19:13:36 +0200

    commit 2

---
+++ src/store/ChuckModule.js
@@ -0,0 +1,12 @@
+export const ChuckModule = {
+    state: {
+        counter: 1
+    },
+    getters: {
+        getCounter(state){
+            return state.counter
+        }
+    },
+    mutations: {},
+    actions: {}
+}
